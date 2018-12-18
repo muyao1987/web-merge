c243cf07c39ae32eb152590d8cea79e1aa77b217
muyao520 <muyao1987@qq.com>
2018-12-18 11:37:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#MarsGIS For Cesium 地图框架 类库源码
+
+1、请安装NPM相关包
+npm install (建议用cnpm install)
+
+2、打开gulpfile.js按示例提示，自行按实际修改src文件
+
+
+3、编译合并指定文件
+npm run build 
+
+
+如有问题请联系木遥QQ： 516584683
+ 
+---------------------------
+三维地图请点击： [http://cesium.marsgis.cn](http://cesium.marsgis.cn)
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano'); 
+
+
+//压缩混淆 
+gulp.task('build', function () {
+    //合并几个js到一个js中
+    gulp.src([
+        "src/lib/Cesium/Cesium.js",
+        "src/lib/cesium-mars/mars3d.js",
+        "src/lib/CesiumPlugins/navigation/viewerCesiumNavigationMixin.min.js",
+    ])
+    .pipe(concat('Cesium.js')) //输出的文件名称
+    //.pipe(uglify()) //需要混淆时取消注释
+    .pipe(gulp.dest('dist/'));
+
+
+    //合并几个css到一个css中
+    gulp.src([
+        "src/lib/Cesium/Widgets/widgets.css",
+        "src/lib/cesium-mars/mars3d.css",
+        "src/lib/CesiumPlugins/navigation/viewerCesiumNavigationMixin.css",
+    ])
+    .pipe(concat('widgets.css')) //输出的文件名称
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/'));
+
+});
+
